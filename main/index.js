1d927b1b3a42257f043c6d13b050a15586d759e4
Andrew Kurylyk <andronniy7@gmail.com>
2025-03-23 18:28:34 +0200

    First

---
+++ main/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let a = 'hello world';
+let b = 'hello world';
+let c = 'hello world';
+console.log(a, b, c);
---
+++ main/index.ts
@@ -0,0 +1,4 @@
+let a:string = 'hello world';
+let b:string = 'hello world';
+let c:string = 'hello world';
+console.log(a,b,c);
